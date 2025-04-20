418acd4581556e824cbb64733470eb119b9051a8
masoud momen <masoudmomen@hotmail.com>
2025-04-20 18:48:49 +0330

    Add project files.

---
+++ Rehab.Application/Contexts/IDatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rehab.Domain.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rehab.Application.Contexts
+{
+    public interface IDatabaseContext
+    {
+        DbSet<User> Users { get; set; }
+        int SaveChanges();
+        int SaveChanges(bool acceptAllChangesOnSuccess);
+        Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default);
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ Rehab.Persistence/Contexts/DatabaseContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rehab.Application.Contexts;
+using Rehab.Domain.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rehab.Persistence.Contexts
+{
+    public class DatabaseContext: DbContext, IDatabaseContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options):base(options)
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        //DbSets : 
+        public DbSet<User> Users { get; set; }
+    }
+}
