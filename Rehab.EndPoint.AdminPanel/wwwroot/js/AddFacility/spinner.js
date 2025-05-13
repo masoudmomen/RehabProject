86f6779c5770fd7f3257ae0b435daab087091709
masoud momen <masoudmomen@hotmail.com>
2025-05-13 21:33:18 +0330

    1404-02-23-server

---
+++ Rehab.EndPoint.AdminPanel/wwwroot/js/AddFacility/spinner.js
@@ -0,0 +1,16 @@
+﻿alert();
+
+var spinner = function () {
+    setTimeout(function () {
+        if ($('#spinner').length > 0) {
+            $('#spinner').removeClass('show');
+        }
+    }, 1);
+};
+
+
+spinner();
+
+
+
+
---
+++ Rehab.Persistence/Migrations/20250513155111_addInsurance.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Rehab.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class addInsurance : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "State",
+                table: "Facilities",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "City",
+                table: "Facilities",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<short>(
+                name: "Founded",
+                table: "Facilities",
+                type: "smallint",
+                nullable: false,
+                defaultValue: (short)0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsVerified",
+                table: "Facilities",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Occupancy",
+                table: "Facilities",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "WhoWeTreat",
+                table: "Facilities",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "Insurances",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Logo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Insurances", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FacilityInsurance",
+                columns: table => new
+                {
+                    FacilitiesId = table.Column<int>(type: "int", nullable: false),
+                    InsurancesId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FacilityInsurance", x => new { x.FacilitiesId, x.InsurancesId });
+                    table.ForeignKey(
+                        name: "FK_FacilityInsurance_Facilities_FacilitiesId",
+                        column: x => x.FacilitiesId,
+                        principalTable: "Facilities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_FacilityInsurance_Insurances_InsurancesId",
+                        column: x => x.InsurancesId,
+                        principalTable: "Insurances",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FacilityInsurance_InsurancesId",
+                table: "FacilityInsurance",
+                column: "InsurancesId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FacilityInsurance");
+
+            migrationBuilder.DropTable(
+                name: "Insurances");
+
+            migrationBuilder.DropColumn(
+                name: "Founded",
+                table: "Facilities");
+
+            migrationBuilder.DropColumn(
+                name: "IsVerified",
+                table: "Facilities");
+
+            migrationBuilder.DropColumn(
+                name: "Occupancy",
+                table: "Facilities");
+
+            migrationBuilder.DropColumn(
+                name: "WhoWeTreat",
+                table: "Facilities");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "State",
+                table: "Facilities",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "City",
+                table: "Facilities",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
