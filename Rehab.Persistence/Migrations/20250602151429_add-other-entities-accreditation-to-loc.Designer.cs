9a3afb9f840819996442e0430ff841edc3807eb9
masoud momen <masoudmomen@hotmail.com>
2025-06-02 21:02:38 +0330

    1404-03-12-server

--- Rehab.Persistence/Migrations/20250602151429_add-other-entities-accreditation-to-loc.Designer.cs
+++ Rehab.Persistence/Migrations/20250602151429_add-other-entities-accreditation-to-loc.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Rehab.Persistence.Contexts;
 
@@ -11,9 +12,11 @@ using Rehab.Persistence.Contexts;
 namespace Rehab.Persistence.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20250602151429_add-other-entities-accreditation-to-loc")]
+    partial class addotherentitiesaccreditationtoloc
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
