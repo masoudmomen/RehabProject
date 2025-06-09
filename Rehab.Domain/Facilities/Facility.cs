0708788eb29694e99a74288da8d396729e99c091
masoud momen <masoudmomen@hotmail.com>
2025-06-09 20:22:45 +0330

    1404-03-19-server

--- Rehab.Domain/Facilities/Facility.cs
+++ Rehab.Domain/Facilities/Facility.cs
@@ -15,14 +15,17 @@ namespace Rehab.Domain.Facilities
         public string City { get; set; } = string.Empty;
         public string State { get; set; } = string.Empty;
         public string Address { get; set; } = string.Empty;
-        public short? WhoWeTreat { get; set; } 
         public string Description { get; set; } = string.Empty;
+        public string Logo { get; set; } = string.Empty;
+        public string Cover { get; set; } = string.Empty;
         public string PhoneNumber { get; set; } = string.Empty;
         public string WebSite { get; set; } = string.Empty;
+        public string Slug { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public bool IsVerified { get; set; } = false;
         public short Founded { get; set; }
-        public string Occupancy { get; set; } = string.Empty;
+        public short OccupancyMin { get; set; } 
+        public short OccupancyMax { get; set; } 
         public ICollection<Insurance>? Insurances { get; set; }
 
     }
