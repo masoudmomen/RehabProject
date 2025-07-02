2c5dd78f41a4e59e5158ef1aa276e654408ae122
masoud momen <masoudmomen@hotmail.com>
2025-07-02 12:57:11 +0330

    1404-04-11-office

--- Rehab.EndPoint.AdminPanel/Viewmodels/FacilityViewmodel.cs
+++ Rehab.EndPoint.AdminPanel/Viewmodels/FacilityViewmodel.cs
@@ -13,23 +13,25 @@ namespace Rehab.EndPoint.AdminPanel.Viewmodels
     public class FacilityViewmodel
     {
         public int Id { get; set; }
-        [Required(ErrorMessage ="Name field is Required")]
         public string Name { get; set; } = string.Empty;
-        public string City { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
         public string State { get; set; } = string.Empty;
-        public string Address { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
-        public string Logo { get; set; } = string.Empty;
-        public string Cover { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
         public string PhoneNumber { get; set; } = string.Empty;
         public string WebSite { get; set; } = string.Empty;
-        public string Slug { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
         public string ProvidersPolicy { get; set; } = string.Empty;
-        public bool IsVerified { get; set; } = false;
         public short Founded { get; set; }
         public short OccupancyMin { get; set; }
         public short OccupancyMax { get; set; }
+        public string Address { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+
+
+        public string Logo { get; set; } = string.Empty;
+        public string Cover { get; set; } = string.Empty;
+        public string Slug { get; set; } = string.Empty;
+        public bool IsVerified { get; set; } = false;
+
         public List<InsuranceViewmodel>? Insurances { get; set; } = new();
         public List<AccreditationViewmodel>? Accreditations { get; set; } = new();
         public List<AmenityViewmodel>? Amenities { get; set; } = new();
