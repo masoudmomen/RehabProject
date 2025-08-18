3d46c997cbf6a09cd418ec9225bdc637178ac1dd
maryam.nabavi <maryam.s.nabavi@gmail.com>
2025-08-19 00:21:11 +0330

    Footer links bugs is fixed

---
+++ Rehab.EndPoint.Web/wwwroot/js/site.js
@@ -0,0 +1,22 @@
+﻿window.footerScrollWatcher = {
+    init: function (dotNetHelper) {
+        window.addEventListener('scroll', function () {
+            const footer = document.getElementById('footer');
+
+            const scrollHeight = document.documentElement.scrollHeight;
+            const scrollTop = window.scrollY || document.documentElement.scrollTop;
+            const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+
+            const expanded = (scrollTop + windowHeight >= scrollHeight - 5);
+         
+            if (expanded) {
+                footer.classList.add('expanded');
+            } else {
+                footer.classList.remove('expanded');
+            }
+
+            dotNetHelper.invokeMethodAsync("SetFooterExpanded", expanded);
+
+        });
+    }
+};
