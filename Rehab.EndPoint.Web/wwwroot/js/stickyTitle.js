266db3a0c96e34b7f00937def25cd26b9797ceff
maryam.nabavi <maryam.s.nabavi@gmail.com>
2025-10-14 10:10:28 +0330

    Section title in FacilityDetails:  -Stick on top on ths scroll

---
+++ Rehab.EndPoint.Web/wwwroot/js/stickyTitle.js
@@ -0,0 +1,22 @@
+﻿
+document.addEventListener("DOMContentLoaded", function () {
+    const titles = document.querySelectorAll(".section-title");
+
+    const observer = new IntersectionObserver(
+        entries => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    titles.forEach(t => t.classList.remove("active"));
+                    entry.target.classList.add("active");
+                }
+            });
+        },
+        {
+            root: null,
+            rootMargin: "0px",
+            threshold: 0.5
+        }
+    );
+
+    titles.forEach(title => observer.observe(title));
+});
