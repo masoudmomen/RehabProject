da2bbe67916d8157aafac54a2e97a6aae86e6bc4
masoud momen <masoudmomen@hotmail.com>
2025-11-12 14:01:56 +0330

    1404-08-21-office

--- Rehab.EndPoint.Web/wwwroot/js/stateAlphabetFilter.js
+++ Rehab.EndPoint.Web/wwwroot/js/stateAlphabetFilter.js
@@ -1,5 +1,5 @@
 ﻿const states = [
-    "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut",
+    "Alabama", "alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut",
     "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa",
     "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan",
     "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire",
