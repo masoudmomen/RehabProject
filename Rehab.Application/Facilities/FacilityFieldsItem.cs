832ab356945aeb54a8e8587cd739e8afb286eebb
masoud momen <masoudmomen@hotmail.com>
2026-01-24 22:03:25 +0330

    1404-11-4-server

---
+++ Rehab.Application/Facilities/FacilityFieldsItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rehab.Application.Facilities
+{
+    public class FacilityFieldsItem
+    {
+        public string? Amenity { get; set; } 
+        public string? Insurance { get; set; }
+        public string? Accreditation { get; set; }
+        public string? Condition { get; set; } 
+        public string? Wwt { get; set; } 
+        public string? Treatment { get; set; }
+        public string? Swt { get; set; } 
+        public string? Loc { get; set; } 
+    }
+}
